190d27ed8a4cb9106e56889df37c1c087779bb3d
aftab <aftab28@gmail.com>
2020-04-19 03:13:52 -0500

    vowel names complete

--- comprehension_tutorial/exercises/lists.py
+++ comprehension_tutorial/exercises/lists.py
@@ -1,8 +1,11 @@
 """List comprehension exercises"""
 
 
-def get_vowel_names():
+def get_vowel_names(names):
     """Return a list containing all names given that start with a vowel."""
+    return [name
+            for name in names
+            if name[0].lower() in 'aeiou']
 
 
 def power_list():
