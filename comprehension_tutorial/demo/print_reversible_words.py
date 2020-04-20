de860343170cf2778b56741c66904ee0d29550e6
aftab <aftab28@gmail.com>
2020-04-20 12:12:11 -0500

    refactored to list comprehensions

--- comprehension_tutorial/demo/print_reversible_words.py
+++ comprehension_tutorial/demo/print_reversible_words.py
@@ -1,18 +1,25 @@
 words_over_five_letters = []
 with open('dictionary.txt') as dictionary_file:
-    for line in dictionary_file:
-        word = line.rstrip()
-        if len(word) > 5:
-            words_over_five_letters.append(word)
+    words = [
+        line.rstrip()
+        for line in dictionary_file
+    ]
+
+    words_over_five_letters = [
+        word
+        for word in words
+        if len(word) > 5
+    ]
 
 reversed_words = set()
 for word in words_over_five_letters:
     reversed_words.add(word[::-1])
 
-reversible_words = []
-for word in words_over_five_letters:
-    if word in reversed_words:
-        reversible_words.append(word)
+reversible_words = [
+    word
+    for word in words_over_five_letters
+    if word in reversed_words
+]
 
 for word in reversible_words:
     print(word)
