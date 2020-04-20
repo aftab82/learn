d66680d87a68e887d612dee04406ab3d9cfcdd38
aftab <aftab28@gmail.com>
2020-04-20 12:06:54 -0500

    fixed only half the words printed

--- comprehension_tutorial/demo/print_reversible_words.py
+++ comprehension_tutorial/demo/print_reversible_words.py
@@ -1,13 +1,18 @@
-reversed_words = set()
-reversible_words = []
-
+words_over_five_letters = []
 with open('dictionary.txt') as dictionary_file:
     for line in dictionary_file:
         word = line.rstrip()
         if len(word) > 5:
-            if word in reversed_words:
-                reversible_words.append(word)
-            reversed_words.add(word[::-1])
+            words_over_five_letters.append(word)
+
+reversed_words = set()
+for word in words_over_five_letters:
+    reversed_words.add(word[::-1])
+
+reversible_words = []
+for word in words_over_five_letters:
+    if word in reversed_words:
+        reversible_words.append(word)
 
 for word in reversible_words:
     print(word)
