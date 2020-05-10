f11922f4167cdff2679433f40f4244b2841371ee
aftab <aftab28@gmail.com>
2020-05-10 00:55:22 -0500

    more exercises complete

--- comprehension_tutorial/exercises/more.py
+++ comprehension_tutorial/exercises/more.py
@@ -1,21 +1,42 @@
 """More comprehension exercises"""
+from lists import get_factors
 
 
-def flip_dict():
+def flip_dict(input_dict):
     """Return a new dictionary that maps the original values to the keys."""
+    return {value: key for key, value in input_dict.items()}
 
 
-def get_ascii_codes():
+def get_ascii_codes(words):
     """Return a dictionary mapping the strings to ASCII codes."""
+    return {
+        word: [
+            ord(letter)
+            for letter in word
+            ]
+        for word in words
+    }
 
 
-def dict_from_truple():
+def dict_from_truple(tuple_list):
     """Turn three-item tuples into a dictionary of two-valued tuples."""
+    return {
+        key: (value1, value2)
+        for key, value1, value2 in tuple_list
+    }
 
 
-def dict_from_tuple():
+def dict_from_tuple(tuple_list):
     """Turn multi-item tuples into a dictionary of two-valued tuples."""
+    return {
+        items[0]: items[1:]
+        for items in tuple_list
+    }
 
 
-def get_all_factors():
+def get_all_factors(nums):
     """Return a dictionary mapping numbers to their factors."""
+    return {
+        num: get_factors(num)
+        for num in nums
+    }
